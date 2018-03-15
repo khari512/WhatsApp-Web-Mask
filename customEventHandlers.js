075e480cbcf55b6c7f943f5b1fb44e2121da216c
Harinath kolli <harinath.kolli@gmail.com>
2018-03-15 13:02:27 +0530

    Update customEventHandlers.js

--- customEventHandlers.js
+++ customEventHandlers.js
@@ -1,6 +1,11 @@
    
    //contains list of custom event handkers
+   document.getElementById("encryptBtn").addEventListener("click", encryptMesgs);
+   document.getElementById("hideEmojiBtn").addEventListener("click", hideEmojis);
+   document.getElementById("hideDelStatusBtn").addEventListener("click", hideMesgDeliveruStatusTicks);
+
    function encryptMesgs(){
+      console.log("custom btn click worked");
       var allMesgsSelector = $(".selectable-text");
         allMesgsSelector.hide();
     }
