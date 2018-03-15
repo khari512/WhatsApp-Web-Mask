c1d18b4fc49ce8a88872cc8972bbbd17cca630f2
Harinath kolli <harinath.kolli@gmail.com>
2018-03-15 12:55:10 +0530

    Create customEventHandlers.js

---
+++ customEventHandlers.js
@@ -0,0 +1,17 @@
+   
+   //contains list of custom event handkers
+   function encryptMesgs(){
+      var allMesgsSelector = $(".selectable-text");
+        allMesgsSelector.hide();
+    }
+
+    //hide emoji type like anger,smile
+    function  hideEmojis(){
+      var emojisSelectors = $("._3HFgq");
+      emojisSelectors.hide();
+    }
+
+    function hideMesgDeliveruStatusTicks(){
+     var selector = $("._32uRw");
+        selector.hide();
+    }
